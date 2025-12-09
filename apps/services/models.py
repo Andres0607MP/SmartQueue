cdec0934ed4a631c2615a6079f2e57fc07c71653
Anderson <andersontopaga2018@gmail.com>
2025-12-09 16:56:50 -0500

    Relación agentes-servicios (ManyToMany / tabla intermedia)

--- apps/services/models.py
+++ apps/services/models.py
@@ -6,8 +6,14 @@ User = get_user_model()
 class Service(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True)
-    estimated_time = models.PositiveIntegerField(help_text="Tiempo estimado en minutos")
+    estimated_time = models.PositiveIntegerField()
     category = models.CharField(max_length=100, blank=True)
+
+    agents = models.ManyToManyField(
+        User,
+        related_name="assigned_services",
+        blank=True
+    )
 
     created_at = models.DateTimeField(auto_now_add=True)
 
